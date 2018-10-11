782c976154dc74948eb002454f33c101205b1a6a
ryans <ryans@jlab.org>
2018-10-11 12:03:35 -0400

    Fix bugs

--- src/org/jlab/Trippy.java
+++ src/org/jlab/Trippy.java
@@ -36,6 +36,26 @@ import org.jlab.mya.stream.IntEventStream;
  */
 public class Trippy {
 
+    /*public void Trippy2() throws SQLException, IOException {
+        DataNexus nexus = new OnDemandNexus(Deployment.ops);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                .withZone(ZoneId.systemDefault());
+
+        IntervalService service = new IntervalService(nexus);
+
+        final int MAX_RECOVERY_SECONDS = 3600; // 1 Hour
+        final String masterFsdNodePv = "ISD0I011G";
+        final String hallARecoveryPv = "HLA:bta_bm_present";
+        final String hallBRecoveryPv = "HLB:bta_bm_present";
+        final String hallCRecoveryPv = "HLC:bta_bm_present";
+        final String hallDRecoveryPv = "HLD:bta_bm_present";
+
+        Instant begin = LocalDateTime.parse("2017-01-01T00:00:00.123456").atZone(ZoneId.systemDefault()).toInstant();
+        Instant end = LocalDateTime.parse("2019-01-01T00:01:00.123456").atZone(ZoneId.systemDefault()).toInstant();
+
+        TreeSet<Instant> masterRecoverySet = getBinaryPoint(service, masterFsdNodePv, begin, end, true);        
+    }*/
     public Trippy() throws SQLException, IOException {
         DataNexus nexus = new OnDemandNexus(Deployment.ops);
 
@@ -54,10 +74,7 @@ public class Trippy {
         Instant begin = LocalDateTime.parse("2017-01-01T00:00:00.123456").atZone(ZoneId.systemDefault()).toInstant();
         Instant end = LocalDateTime.parse("2019-01-01T00:01:00.123456").atZone(ZoneId.systemDefault()).toInstant();
 
-        //TreeSet<Instant> masterRecoverySet = getBinaryPoint(service, masterFsdNodePv, begin, end, false);
-        //TreeSet<Instant> masterTripSet = getBinaryPoint(service, masterFsdNodePv, begin, end, true);
-        
-        List<TreeSet<Instant>> masterPoints = getBinaryPoint(service, masterFsdNodePv, begin, end);
+        List<TreeSet<Instant>> masterPoints = getFsdMasterPoints(service, masterFsdNodePv, begin, end);
         TreeSet<Instant> masterRecoverySet = masterPoints.get(0);
         TreeSet<Instant> masterTripSet = masterPoints.get(1);
 
@@ -81,23 +98,21 @@ public class Trippy {
         row1.createCell(2).setCellValue("HALL A RECOVERY");
         row1.createCell(3).setCellValue("HALL B RECOVERY");
         row1.createCell(4).setCellValue("HALL C RECOVERY");
-        row1.createCell(5).setCellValue("TRIP SECONDS");
-        row1.createCell(6).setCellValue("HALL A RECOVERY SECONDS");
-        row1.createCell(7).setCellValue("HALL B RECOVERY SECONDS");
-        row1.createCell(8).setCellValue("HALL C RECOVERY SECONDS");
-        row1.createCell(9).setCellValue("HALL D RECOVERY SECONDS");        
-        
-        
+        row1.createCell(5).setCellValue("HALL D RECOVERY");
+        row1.createCell(6).setCellValue("TRIP SECONDS");
+        row1.createCell(7).setCellValue("A RECOVERY SECONDS");
+        row1.createCell(8).setCellValue("B RECOVERY SECONDS");
+        row1.createCell(9).setCellValue("C RECOVERY SECONDS");
+        row1.createCell(10).setCellValue("D RECOVERY SECONDS");
+
         //((XSSFSheet) sheet1).getColumnHelper().setColDefaultStyle(0, dateStyle);
         //((XSSFSheet) sheet1).getColumnHelper().setColDefaultStyle(1, dateStyle);
-        
         for (Instant tripClear : masterRecoverySet) {
 
             Instant tripDown = masterTripSet.lower(tripClear);
             Instant nextTrip = masterTripSet.higher(tripClear);
 
             //System.out.println(tripDown);
-            
             Instant hallARecoveryEnd = hallARecoverySet.higher(tripClear);
             Instant hallBRecoveryEnd = hallBRecoverySet.higher(tripClear);
             Instant hallCRecoveryEnd = hallCRecoverySet.higher(tripClear);
@@ -105,19 +120,19 @@ public class Trippy {
 
             if (nextTrip != null) {
                 if (hallARecoveryEnd != null && nextTrip.getEpochSecond() < hallARecoveryEnd.getEpochSecond()) {
-                    hallARecoveryEnd = nextTrip;
+                    hallARecoveryEnd = null;
                 }
 
                 if (hallBRecoveryEnd != null && nextTrip.getEpochSecond() < hallBRecoveryEnd.getEpochSecond()) {
-                    hallBRecoveryEnd = nextTrip;
+                    hallBRecoveryEnd = null;
                 }
 
                 if (hallCRecoveryEnd != null && nextTrip.getEpochSecond() < hallCRecoveryEnd.getEpochSecond()) {
-                    hallCRecoveryEnd = nextTrip;
+                    hallCRecoveryEnd = null;
                 }
 
                 if (hallDRecoveryEnd != null && nextTrip.getEpochSecond() < hallDRecoveryEnd.getEpochSecond()) {
-                    hallDRecoveryEnd = nextTrip;
+                    hallDRecoveryEnd = null;
                 }
             }
 
@@ -130,7 +145,7 @@ public class Trippy {
             Row row = sheet1.createRow(rowCounter++);
 
             Cell c;
-            
+
             if (tripDown != null) {
                 c = row.createCell(0);
                 c.setCellValue(Date.from(tripDown));
@@ -140,45 +155,55 @@ public class Trippy {
             c = row.createCell(1);
             c.setCellValue(Date.from(tripClear));
             c.setCellStyle(dateStyle);
-            
+
             if (tripDown != null && tripClear != null) {
                 Duration tripRecovery = Duration.between(tripDown, tripClear);
-                row.createCell(5).setCellValue(tripRecovery.getSeconds());
+                row.createCell(6).setCellValue(tripRecovery.getSeconds());
             }
 
             //System.out.println(formatter.format(tripClear) + " - ");
             if (hallARecovery != null && hallARecovery.getSeconds() < MAX_RECOVERY_SECONDS) {
-                row.createCell(2).setCellValue(formatter.format(hallARecoveryEnd));
-                row.createCell(6).setCellValue(hallARecovery.getSeconds());
+                c = row.createCell(2);
+                c.setCellValue(Date.from(hallARecoveryEnd));
+                c.setCellStyle(dateStyle);
+                row.createCell(7).setCellValue(hallARecovery.getSeconds());
             }
 
             if (hallBRecovery != null && hallBRecovery.getSeconds() < MAX_RECOVERY_SECONDS) {
-                row.createCell(3).setCellValue(formatter.format(hallBRecoveryEnd));
-                row.createCell(7).setCellValue(hallBRecovery.getSeconds());
+                c = row.createCell(3);
+                c.setCellValue(Date.from(hallBRecoveryEnd));
+                c.setCellStyle(dateStyle);
+                row.createCell(8).setCellValue(hallBRecovery.getSeconds());
             }
 
             if (hallCRecovery != null && hallCRecovery.getSeconds() < MAX_RECOVERY_SECONDS) {
-                row.createCell(3).setCellValue(formatter.format(hallCRecoveryEnd));
-                row.createCell(7).setCellValue(hallCRecovery.getSeconds());
+                c = row.createCell(4);
+                c.setCellValue(Date.from(hallCRecoveryEnd));
+                c.setCellStyle(dateStyle);
+                row.createCell(9).setCellValue(hallCRecovery.getSeconds());
             }
 
             if (hallDRecovery != null && hallDRecovery.getSeconds() < MAX_RECOVERY_SECONDS) {
-                row.createCell(3).setCellValue(formatter.format(hallDRecoveryEnd));
-                row.createCell(7).setCellValue(hallDRecovery.getSeconds());
+                c = row.createCell(5);
+                c.setCellValue(Date.from(hallDRecoveryEnd));
+                c.setCellStyle(dateStyle);
+                row.createCell(10).setCellValue(hallDRecovery.getSeconds());
             }
         }
 
-        /*sheet1.autoSizeColumn(0);
-        sheet1.autoSizeColumn(1);
-        sheet1.autoSizeColumn(2);
-        sheet1.autoSizeColumn(3);
-        sheet1.autoSizeColumn(4);
-        sheet1.autoSizeColumn(5);
-        sheet1.autoSizeColumn(6);
-        sheet1.autoSizeColumn(7);
-        sheet1.autoSizeColumn(8);
-        sheet1.autoSizeColumn(9);*/
-        
+        // width is number of chars X 256
+        sheet1.setColumnWidth(0, 20 * 256);
+        sheet1.setColumnWidth(1, 20 * 256);
+        sheet1.setColumnWidth(2, 20 * 256);
+        sheet1.setColumnWidth(3, 20 * 256);
+        sheet1.setColumnWidth(4, 20 * 256);
+        sheet1.setColumnWidth(5, 20 * 256);
+        sheet1.setColumnWidth(6, 20 * 256);
+        sheet1.setColumnWidth(7, 20 * 256);
+        sheet1.setColumnWidth(8, 20 * 256);
+        sheet1.setColumnWidth(9, 20 * 256);
+        sheet1.setColumnWidth(10, 20 * 256);
+
         try (FileOutputStream out = new FileOutputStream("trips.xlsx")) {
             wb.write(out);
         }
@@ -205,10 +230,7 @@ public class Trippy {
 
             while ((event = stream.read()) != null) {
 
-                System.out.println(event.getValue());
-                
                 if ((one && event.getValue() == 1) || (!one && event.getValue() == 0)) {
-                    System.out.println("Adding value");
                     set.add(event.getTimestampAsInstant());
                 }
             }
@@ -217,22 +239,31 @@ public class Trippy {
         return set;
     }
 
-    private List<TreeSet<Instant>> getBinaryPoint(IntervalService service, String pv, Instant begin, Instant end) throws SQLException, IOException {
+    private List<TreeSet<Instant>> getFsdMasterPoints(IntervalService service, String pv, Instant begin, Instant end) throws SQLException, IOException {
         Metadata metadata = service.findMetadata(pv);
         IntervalQueryParams params = new IntervalQueryParams(metadata, begin, end);
 
         TreeSet<Instant> setOne = new TreeSet<>();
         TreeSet<Instant> setZero = new TreeSet<>();
 
+        // 0 and 1 values do not alternate as it is possible for a trip to occur during another trip as a trip just means a new non-zero value
+        // However, we only care about first value change that started trip so we track changes skip trips-in-a-trip
+        boolean inTrip = false;
+        
         try (IntEventStream stream = service.openIntStream(params)) {
 
             IntEvent event;
 
             while ((event = stream.read()) != null) {
-                if (event.getValue() == 1) {
-                    setOne.add(event.getTimestampAsInstant());
-                } else if (event.getValue() == 0) {
+                
+                if (event.getValue() > 0) { // Not actually 1, and usuaully is just non zero integer
+                    if(!inTrip) { // Skip a trip-in-a-trip
+                        setOne.add(event.getTimestampAsInstant());
+                        inTrip = true;
+                    }
+                } else {
                     setZero.add(event.getTimestampAsInstant());
+                    inTrip = false;
                 }
             }
         }
